b47aa1f14ab8b58c67d4a96e7ff4da723c3b0feb
BuildTools <williamcaetano14@gmail.com>
2021-09-06 18:10:10 -0300

    :lipstick: Added custom theme colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react"
+// 1. Import the extendTheme function
+import { extendTheme, theme } from "@chakra-ui/react"
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  misthy: theme.colors.purple,
+  liw: theme.colors.yellow,
+  myu: theme.colors.red,
+  primary: theme.colors.pink,
+  secondary: theme.colors.green,
+}
+
+const customTheme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
