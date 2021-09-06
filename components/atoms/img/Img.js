11f8bf23bd22af70a25c55fd30f8c4f17f3b83d5
BuildTools <williamcaetano14@gmail.com>
2021-09-06 18:37:35 -0300

    :sparkles: Added chakra factory on image

--- components/atoms/img/Img.js
+++ components/atoms/img/Img.js
@@ -1,5 +1,8 @@
+import { chakra } from '@chakra-ui/system'
 import Image from 'next/image'
 
+const NextImage = chakra(Image);
+
 export default function Img({alt, ...props}) {
-    return <Image alt={alt ? "" : alt} {...props} />
+    return <NextImage alt={alt ? "" : alt} {...props} />
 }
