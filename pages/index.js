b9ac3f0e754979ab93d1a48be0eff7a45e434eac
BuildTools <williamcaetano14@gmail.com>
2021-09-14 20:50:58 -0300

    :wrench: Ajusted lint

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import {Box, FaTwitch} from '../components'
-import StreamerCard from '../components/molecules/streamerCard'
+import Head from 'next/head';
+import { Box, FaTwitch } from '../components';
+import StreamerCard from '../components/molecules/streamerCard';
 import misthyPicture from '../assets/misthy.svg';
 
 export default function Home() {
@@ -8,11 +8,22 @@ export default function Home() {
     <div>
       <Head>
         <title>Bingo Girls</title>
-        <meta name="description" content="Site de Liwphael, Myumii e Misthy. Streamer da Twitch" />
+        <meta
+          name="description"
+          content="Site de Liwphael, Myumii e Misthy. Streamer da Twitch"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box> <FaTwitch /> test </Box>
-      <StreamerCard alt="Misthy" bgColor="misthy" headingText="Misthy" imgSrc={misthyPicture} links={["test"]}/>
+      <Box>
+        <FaTwitch /> test
+      </Box>
+      <StreamerCard
+        alt="Misthy"
+        bgColor="misthy"
+        headingText="Misthy"
+        imgSrc={misthyPicture}
+        links={['test']}
+      />
     </div>
-  )
+  );
 }
