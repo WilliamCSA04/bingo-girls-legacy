5fa05bcfe6fdebc7f757bb48aebe47e338e6f4d4
BuildTools <williamcaetano14@gmail.com>
2021-10-01 20:11:20 -0300

    :bento: Moved all assets to public page

--- components/molecules/StreamerCard/StreamerCard.stories.js
+++ components/molecules/StreamerCard/StreamerCard.stories.js
@@ -10,9 +10,7 @@ export const MisthyStory = () => (
     alt="Misthy"
     bgColor="misthy"
     headingText="Misthy"
-    imgSrc={
-      'https://e7.pngegg.com/pngimages/102/481/png-clipart-anime-desktop-manga-television-show-anime-black-hair-manga-thumbnail.png'
-    }
+    imgSrc={'/assets/misthy.svg'}
     links={[
       'https://e7.pngegg.com/pngimages/102/481/png-clipart-anime-desktop-manga-television-show-anime-black-hair-manga-thumbnail.png',
     ]}
