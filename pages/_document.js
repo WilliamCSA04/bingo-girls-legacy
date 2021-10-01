ea6125d2255cfb41916e385b89d7cdbfe0dc8f14
BuildTools <williamcaetano14@gmail.com>
2021-10-01 20:26:37 -0300

    :zap: Added font optimization

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+// TODO: Update it to a version witout this error
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="Site de Liwphael, Myumii e Misthy. Streamer da Twitch"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,11 @@
 import Head from 'next/head';
-import { Box, FaTwitch } from '../components';
-import StreamerCard from '../components/molecules/StreamerCard';
-import misthyPicture from '../assets/misthy.svg';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Bingo Girls</title>
-        <meta
-          name="description"
-          content="Site de Liwphael, Myumii e Misthy. Streamer da Twitch"
-        />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Home | Bingo Girls</title>
       </Head>
-      <Box>
-        <FaTwitch /> test
-      </Box>
-      <StreamerCard
-        alt="Misthy"
-        bgColor="misthy"
-        headingText="Misthy"
-        imgSrc={misthyPicture}
-        links={['test']}
-      />
     </div>
   );
 }
