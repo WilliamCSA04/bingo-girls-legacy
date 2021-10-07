fd0e562c8c14055cf2af528dbe5b5085ee1009c3
BuildTools <williamcaetano14@gmail.com>
2021-10-07 20:19:21 -0300

    :sparkles: Created VStack

---
+++ components/atoms/VStack/VStack.js
@@ -0,0 +1,1 @@
+export { VStack as default } from '@chakra-ui/react';
--- components/atoms/index.js
+++ components/atoms/index.js
@@ -1,8 +1,9 @@
-export { default as Box } from './Box'
-export { default as Img } from './Img'
-export { default as Text } from './Text'
-export { default as Heading } from './Heading'
-export { default as Flex } from './Flex'
-export { default as Figure } from './Figure'
-export { default as Figcaption } from './Figcaption'
-export * from './FaIcons'
+export { default as Box } from './Box';
+export { default as Img } from './Img';
+export { default as Text } from './Text';
+export { default as Heading } from './Heading';
+export { default as Flex } from './Flex';
+export { default as Figure } from './Figure';
+export { default as Figcaption } from './Figcaption';
+export { default as VStack } from './VStack';
+export * from './FaIcons';
