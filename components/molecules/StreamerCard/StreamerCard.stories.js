bf162a7812b3a13319f8b5fc4626bfe5cc8866e3
BuildTools <williamcaetano14@gmail.com>
2021-10-08 19:48:28 -0300

    :sparkles: Written more stories

--- components/molecules/StreamerCard/StreamerCard.stories.js
+++ components/molecules/StreamerCard/StreamerCard.stories.js
@@ -19,3 +19,45 @@ export const MisthyStory = () => (
     ]}
   />
 );
+
+export const LiwphaelStory = () => (
+  <StreamerCard
+    alt="Liwphael"
+    bgColor="liw.500"
+    headingText="Liwphael"
+    imgSrc={'/assets/liwphael.svg'}
+    links={[
+      {
+        text: 'Canal de Lives',
+        link: 'https://www.twitch.tv/liwphael/',
+      },
+      {
+        text: 'Youtube',
+        link: 'https://www.youtube.com/channel/UCkSCh6KYHbdpfJJVJyGBhZw',
+      },
+    ]}
+  />
+);
+
+export const MyumiiStory = () => (
+  <StreamerCard
+    alt="Myumii"
+    bgColor="myu.500"
+    headingText="Myumii"
+    imgSrc={'/assets/myumii.svg'}
+    links={[
+      {
+        text: 'Canal de Lives',
+        link: 'https://www.twitch.tv/myumii/',
+      },
+      {
+        text: 'Youtube',
+        link: 'https://www.youtube.com/c/MyumiiFox',
+      },
+      {
+        text: 'Clipes',
+        link: 'https://www.youtube.com/c/LivesdaMyu',
+      },
+    ]}
+  />
+);
