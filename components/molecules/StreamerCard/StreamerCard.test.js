66dff7285e1de30cd622e29b76ec0bf45618390f
BuildTools <williamcaetano14@gmail.com>
2021-10-08 18:40:18 -0300

    :white_check_mark: Added link test for streamer card

--- components/molecules/StreamerCard/StreamerCard.test.js
+++ components/molecules/StreamerCard/StreamerCard.test.js
@@ -8,7 +8,7 @@ const Wrapper = ({ children }) => (
 );
 
 describe('Streamer Card test', () => {
-  it('Should successfully ', () => {
+  it('Should successfully render', () => {
     render(
       <Wrapper>
         <StreamerCard
@@ -32,5 +32,39 @@ describe('Streamer Card test', () => {
     expect(heading).toBeInTheDocument();
     expect(image).toBeInTheDocument();
     expect(links).toHaveLength(1);
+  });
+
+  it('Should have all links with href', () => {
+    const linksMock = [
+      {
+        text: 'Canal de Lives',
+        link: 'https://www.twitch.tv/misthy/',
+      },
+      {
+        text: 'Canal de Clipes',
+        link: 'https://www.youtube.com/channel/UCHOmht_HY5IoJgCCJeIw6Mw',
+      },
+      {
+        text: 'Insta',
+        link: 'https://www.instagram.com/miisthy_/',
+      },
+    ];
+    render(
+      <Wrapper>
+        <StreamerCard
+          alt="Misthy"
+          bgColor="misthy.500"
+          headingText="Misthy"
+          imgSrc={'/assets/misthy.svg'}
+          links={linksMock}
+        />
+      </Wrapper>
+    );
+    const links = screen.getAllByRole('link');
+
+    expect(links).toHaveLength(linksMock.length);
+    links.forEach((link, index) => {
+      expect(link).toHaveAttribute('href', linksMock[index].link);
+    });
   });
 });
