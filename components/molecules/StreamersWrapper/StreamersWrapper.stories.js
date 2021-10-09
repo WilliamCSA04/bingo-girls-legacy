a3e6e30d44aa967d077e6de336d419d717f315b0
BuildTools <williamcaetano14@gmail.com>
2021-10-09 03:05:27 -0300

    :recycle: StreamersWrapper stories

--- components/molecules/StreamersWrapper/StreamersWrapper.stories.js
+++ components/molecules/StreamersWrapper/StreamersWrapper.stories.js
@@ -6,55 +6,67 @@ export default {
   component: StreamerWrapper,
 };
 
+const MisthyCard = () => (
+  <StreamerCard
+    alt="Misthy"
+    bgColor="misthy.500"
+    headingText="Misthy"
+    imgSrc={'/assets/misthy.svg'}
+    links={[
+      {
+        text: 'Canal de Lives',
+        link: 'https://www.twitch.tv/misthy/',
+      },
+    ]}
+  />
+);
+
+const LiwphaelCard = () => (
+  <StreamerCard
+    alt="Liwphael"
+    bgColor="liw.500"
+    headingText="Liwphael"
+    imgSrc={'/assets/liwphael.svg'}
+    links={[
+      {
+        text: 'Canal de Lives',
+        link: 'https://www.twitch.tv/liwphael/',
+      },
+      {
+        text: 'Youtube',
+        link: 'https://www.youtube.com/channel/UCkSCh6KYHbdpfJJVJyGBhZw',
+      },
+    ]}
+  />
+);
+
+const MyumiiCard = () => (
+  <StreamerCard
+    alt="Myumii"
+    bgColor="myu.500"
+    headingText="Myumii"
+    imgSrc={'/assets/myumii.svg'}
+    links={[
+      {
+        text: 'Canal de Lives',
+        link: 'https://www.twitch.tv/myumii/',
+      },
+      {
+        text: 'Youtube',
+        link: 'https://www.youtube.com/c/MyumiiFox',
+      },
+      {
+        text: 'Clipes',
+        link: 'https://www.youtube.com/c/LivesdaMyu',
+      },
+    ]}
+  />
+);
+
 export const AllStreamers = () => (
   <StreamerWrapper>
-    <StreamerCard
-      alt="Misthy"
-      bgColor="misthy.500"
-      headingText="Misthy"
-      imgSrc={'/assets/misthy.svg'}
-      links={[
-        {
-          text: 'Canal de Lives',
-          link: 'https://www.twitch.tv/misthy/',
-        },
-      ]}
-    />
-    <StreamerCard
-      alt="Liwphael"
-      bgColor="liw.500"
-      headingText="Liwphael"
-      imgSrc={'/assets/liwphael.svg'}
-      links={[
-        {
-          text: 'Canal de Lives',
-          link: 'https://www.twitch.tv/liwphael/',
-        },
-        {
-          text: 'Youtube',
-          link: 'https://www.youtube.com/channel/UCkSCh6KYHbdpfJJVJyGBhZw',
-        },
-      ]}
-    />
-    <StreamerCard
-      alt="Myumii"
-      bgColor="myu.500"
-      headingText="Myumii"
-      imgSrc={'/assets/myumii.svg'}
-      links={[
-        {
-          text: 'Canal de Lives',
-          link: 'https://www.twitch.tv/myumii/',
-        },
-        {
-          text: 'Youtube',
-          link: 'https://www.youtube.com/c/MyumiiFox',
-        },
-        {
-          text: 'Clipes',
-          link: 'https://www.youtube.com/c/LivesdaMyu',
-        },
-      ]}
-    />
+    <MisthyCard />
+    <LiwphaelCard />
+    <MyumiiCard />
   </StreamerWrapper>
 );
