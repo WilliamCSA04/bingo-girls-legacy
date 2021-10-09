7e75b4f51000c315c6feff8bf776a835edb0b246
BuildTools <williamcaetano14@gmail.com>
2021-10-08 22:12:00 -0300

    :sparkles: Added title to Section

--- components/organisms/Section/Section.js
+++ components/organisms/Section/Section.js
@@ -1,5 +1,23 @@
-import { Section as ChakraSection } from '@chakra-ui/react';
+import PropTypes from './PropTypes';
+import { Section as ChakraSection, VisuallyHidden } from '@chakra-ui/react';
+import { Heading } from '../../atoms';
 
-export default function Section({ children }) {
-  return <ChakraSection>{children}</ChakraSection>;
+export default function Section({ children, title = '', hiddenTitle = false }) {
+  return (
+    <ChakraSection>
+      {hiddenTitle ? (
+        <VisuallyHidden>
+          <Heading>{title}</Heading>
+        </VisuallyHidden>
+      ) : (
+        <Heading>{title}</Heading>
+      )}
+      {children}
+    </ChakraSection>
+  );
 }
+
+Section.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+};
