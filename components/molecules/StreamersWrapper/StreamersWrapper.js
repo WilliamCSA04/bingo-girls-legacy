30d2e91d1b4390907d418245c38b3604888547f1
BuildTools <williamcaetano14@gmail.com>
2021-10-16 22:09:31 -0300

    :lipstick: Centralized streamers cards

--- components/molecules/StreamersWrapper/StreamersWrapper.js
+++ components/molecules/StreamersWrapper/StreamersWrapper.js
@@ -2,7 +2,7 @@ import { SimpleGrid } from '../../atoms';
 
 export default function StreamersWrapper({ children }) {
   return (
-    <SimpleGrid columns={3} spacing={10} gridAutoRows="1fr">
+    <SimpleGrid columns={3} spacing={10} gridAutoRows="1fr" placeItems="center">
       {children}
     </SimpleGrid>
   );
--- components/organisms/StreamersComposition/StreamersComposition.js
+++ components/organisms/StreamersComposition/StreamersComposition.js
@@ -3,8 +3,8 @@ import { StreamerCard, StreamersWrapper } from '../../molecules';
 export default function StreamerComposition({ data = [] }) {
   return (
     <StreamersWrapper>
-      {data.map((d) => (
-        <StreamerCard key={d.name} {...d} />
+      {data.map((d, index) => (
+        <StreamerCard key={index} {...d} />
       ))}
     </StreamersWrapper>
   );
