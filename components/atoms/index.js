2b2053578a2bac196ac33a49e8f68e3b26a5b1c8
BuildTools <williamcaetano14@gmail.com>
2021-11-01 12:04:51 -0300

    :sparkles: Created Swiper

--- components/atoms/index.js
+++ components/atoms/index.js
@@ -12,3 +12,4 @@ export { default as SimpleGrid } from './SimpleGrid';
 export { default as Switch } from './Switch';
 export { default as Header } from './Header';
 export { default as Icon } from './Icon';
+export { default as Swiper } from './Swiper';
