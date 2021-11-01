3f5b1986639725e7b41eb1a1f02bf2e97cb3c9a3
BuildTools <williamcaetano14@gmail.com>
2021-11-01 11:14:32 -0300

    :adhesive_bandage: Added missing export to ColorModeSwitch

--- components/molecules/index.js
+++ components/molecules/index.js
@@ -1,2 +1,3 @@
 export { default as StreamerCard } from './StreamerCard';
 export { default as StreamersWrapper } from './StreamersWrapper';
+export { default as ColorModeSwitch } from './ColorModeSwitch';
