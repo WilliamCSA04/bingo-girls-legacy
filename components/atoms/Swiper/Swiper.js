0390f6dab6dd67b788162baa65bda4d72b18d38e
BuildTools <williamcaetano14@gmail.com>
2021-11-01 18:44:30 -0300

    :construction: Added list for swiper

--- components/atoms/Swiper/Swiper.js
+++ components/atoms/Swiper/Swiper.js
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types';
+import { Flex } from '@chakra-ui/react';
 export default function Swiper({ children }) {
-  return <div>{children}</div>;
+  return (
+    <Flex as="ul" listStyleType="none">
+      {children}
+    </Flex>
+  );
 }
 
 Swiper.propTypes = {
--- components/atoms/SwiperSlide/SwiperSlide.js
+++ components/atoms/SwiperSlide/SwiperSlide.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
-import { Box } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import { Box, chakra } from '@chakra-ui/react';
 
+const Li = chakra.li;
 export default function SwiperSlide({ children }) {
-  return <Box>{children}</Box>;
+  return <Li>{children}</Li>;
 }
 
 SwiperSlide.propTypes = {
