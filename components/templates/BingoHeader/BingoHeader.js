704b63794fb8640306ef2b89b269d7530cca205e
BuildTools <williamcaetano14@gmail.com>
2021-11-01 11:40:25 -0300

    :speech_balloon: Better const name

--- components/templates/BingoHeader/BingoHeader.js
+++ components/templates/BingoHeader/BingoHeader.js
@@ -2,6 +2,6 @@ import { useColorModeValue } from '@chakra-ui/react';
 import { Header } from '../../organisms';
 
 export default function BingoHeader() {
-  const color = useColorModeValue('pink.300', 'gray.900');
-  return <Header bgColor={color} h="10xs" />;
+  const bgColor = useColorModeValue('pink.300', 'gray.900');
+  return <Header bgColor={bgColor} h="10xs" />;
 }
