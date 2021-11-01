0aba46067da6b507bf2c3ba605dea831b3666dbe
BuildTools <williamcaetano14@gmail.com>
2021-11-01 10:58:33 -0300

    :lipstick: :wheelchair: Ajusted details on Color Mode Switch

--- components/molecules/ColorModeSwitch/ColorModeSwitch.js
+++ components/molecules/ColorModeSwitch/ColorModeSwitch.js
@@ -1,15 +1,33 @@
-import { Icon, useColorMode } from '@chakra-ui/react';
+import { Box, Icon, useColorMode } from '@chakra-ui/react';
 import { Switch, Flex } from '../../atoms';
 import { BsSun, BsSunFill, BsMoon, BsMoonFill } from 'react-icons/bs';
+
+function IconWrapper({ children }) {
+  return (
+    <Flex alignItems={'center'} mx={1}>
+      {children}
+    </Flex>
+  );
+}
 
 export default function ColorModeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode();
   const isLightMode = colorMode === 'light';
+  const mode = !isLightMode ? 'claro' : 'escuro';
   return (
     <Flex>
-      <Icon as={isLightMode ? BsSunFill : BsSun} />
-      <Switch onChange={toggleColorMode} defaultValue={isLightMode} />
-      <Icon as={isLightMode ? BsMoon : BsMoonFill} />
+      <IconWrapper>
+        <Icon as={isLightMode ? BsSunFill : BsSun} color="yellow.400" />
+      </IconWrapper>
+      <Switch
+        onChange={toggleColorMode}
+        defaultValue={!isLightMode}
+        aria-label={`Alterar para modo ${mode}`}
+        colorScheme="green"
+      />
+      <IconWrapper>
+        <Icon as={isLightMode ? BsMoon : BsMoonFill} />
+      </IconWrapper>
     </Flex>
   );
 }
