5a280cf3439112aaaa52cdd9c3761d1050f57955
BuildTools <williamcaetano14@gmail.com>
2021-11-01 18:00:40 -0300

    :recycle: :construction: :heavy_minus_sign: Removed all swiper dependencies

--- components/atoms/Swiper/Swiper.js
+++ components/atoms/Swiper/Swiper.js
@@ -1,47 +1,8 @@
 import PropTypes from 'prop-types';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import { Swiper as ReactSwipper } from 'swiper/react';
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
-export default function Swiper({
-  children,
-  spaceBetween = 50,
-  slidesPerView = 3,
-  pagination = { clickable: true },
-  scrollbar = { draggable: true },
-  onSwiper = (swiper) => {},
-  onSlideChange = () => {},
-}) {
-  return (
-    <ReactSwipper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={spaceBetween}
-      slidesPerView={slidesPerView}
-      navigation
-      pagination={pagination}
-      scrollbar={scrollbar}
-      onSwiper={onSwiper}
-      onSlideChange={onSlideChange}
-    >
-      {children}
-    </ReactSwipper>
-  );
+export default function Swiper({ children }) {
+  return <div>{children}</div>;
 }
 
 Swiper.propTypes = {
   children: PropTypes.node,
-  spaceBetween: PropTypes.number,
-  slidesPerView: PropTypes.number,
-  pagination: PropTypes.shape({
-    clickable: PropTypes.bool,
-  }),
-  scrollbar: PropTypes.shape({
-    draggable: PropTypes.bool,
-  }),
-  onSwiper: PropTypes.func,
-  onSlideChange: PropTypes.func,
 };
--- components/atoms/SwiperSlide/SwiperSlide.js
+++ components/atoms/SwiperSlide/SwiperSlide.js
@@ -1,3 +1,10 @@
-import { SwiperSlide } from 'swiper';
+import PropTypes from 'prop-types';
+import { Box } from '@chakra-ui/react';
 
-export default SwiperSlide;
+export default function SwiperSlide({ children }) {
+  return <Box>{children}</Box>;
+}
+
+SwiperSlide.propTypes = {
+  children: PropTypes.node,
+};
