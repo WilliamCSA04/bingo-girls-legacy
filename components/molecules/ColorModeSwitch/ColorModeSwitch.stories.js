2dd358899367662fe3aac12b5a843dcf3e51cd1c
BuildTools <williamcaetano14@gmail.com>
2021-11-01 10:37:06 -0300

    :sparkles: Created story for color mode switch

---
+++ components/molecules/ColorModeSwitch/ColorModeSwitch.stories.js
@@ -0,0 +1,10 @@
+import ColorModeSwitch from '.';
+
+export default {
+  title: 'Color Mode Switch',
+  component: ColorModeSwitch,
+};
+
+export const Switch = () => {
+  return <ColorModeSwitch />;
+};
