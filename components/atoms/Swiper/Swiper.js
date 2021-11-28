1c93ea6db1c4a40447726569883086969c07dfb1
BuildTools <williamcaetano14@gmail.com>
2021-11-28 00:44:51 -0300

    :recycle: Components now use react elastic carousel

--- components/atoms/Swiper/Swiper.js
+++ components/atoms/Swiper/Swiper.js
@@ -1,11 +1,8 @@
 import PropTypes from 'prop-types';
 import { Flex } from '@chakra-ui/react';
+import Carousel from 'react-elastic-carousel';
 export default function Swiper({ children }) {
-  return (
-    <Flex as="ul" listStyleType="none">
-      {children}
-    </Flex>
-  );
+  return <Carousel>{children}</Carousel>;
 }
 
 Swiper.propTypes = {
--- components/atoms/SwiperSlide/SwiperSlide.js
+++ components/atoms/SwiperSlide/SwiperSlide.js
@@ -4,7 +4,7 @@ import { Box, chakra } from '@chakra-ui/react';
 
 const Li = chakra.li;
 export default function SwiperSlide({ children }) {
-  return <Li>{children}</Li>;
+  return <div>{children}</div>;
 }
 
 SwiperSlide.propTypes = {
