0747a4d54625ef0b579605d8fb1f1659a019d0f0
BuildTools <williamcaetano14@gmail.com>
2021-11-28 03:17:05 -0300

    :recycle: Children logic from generic swiper to swiper

--- components/atoms/Swiper/Swiper.js
+++ components/atoms/Swiper/Swiper.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
+import { Children } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import '../../../node_modules/react-alice-carousel/lib/alice-carousel.css';
 
-export default function Swiper({ items, ...props }) {
-  return <AliceCarousel items={items} {...props} />;
+export default function Swiper({ children, ...props }) {
+  const listOfChildren = Children.toArray(children);
+
+  return <AliceCarousel items={listOfChildren} {...props} />;
 }
 
 Swiper.propTypes = {
--- components/molecules/GenericSwiper/GenericSwiper.js
+++ components/molecules/GenericSwiper/GenericSwiper.js
@@ -1,11 +1,9 @@
-import { Children } from 'react';
 import { Swiper } from '../../atoms';
 
 export default function GenericSwiper({ children, ...props }) {
-  const listOfChildren = Children.toArray(children);
   return (
     <>
-      <Swiper items={listOfChildren} {...props} />
+      <Swiper {...props}>{children}</Swiper>
     </>
   );
 }
