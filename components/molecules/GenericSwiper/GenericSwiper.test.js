555e1b608b3ff527891e5584e5d9f1a4888463ed
BuildTools <williamcaetano14@gmail.com>
2021-11-30 22:41:36 -0300

    :wrench: Configured jest css import

--- components/molecules/GenericSwiper/GenericSwiper.test.js
+++ components/molecules/GenericSwiper/GenericSwiper.test.js
@@ -7,22 +7,20 @@ const Wrapper = ({ children }) => (
   <ChakraProvider theme={customTheme}>{children}</ChakraProvider>
 );
 
-//TOFIX: export { default as Swiper, default } from './core/core.js' => Syntax Error
-
-// describe('Generic Swiper', () => {
-//   it('Should render all children', () => {
-//     render(
-//       <Wrapper>
-//         <GenericSwiper>
-//           <p data-testid="text">1</p>
-//           <p data-testid="text">2</p>
-//           <p data-testid="text">3</p>
-//           <p data-testid="text">4</p>
-//           <p data-testid="text">5</p>
-//         </GenericSwiper>
-//       </Wrapper>
-//     );
-//     const allText = getAllByTestId('text');
-//     expect(allText).toHaveLength(5);
-//   });
-// });
+describe('Generic Swiper', () => {
+  it('Should render all children', () => {
+    render(
+      <Wrapper>
+        <GenericSwiper>
+          <p data-testid="text">1</p>
+          <p data-testid="text">2</p>
+          <p data-testid="text">3</p>
+          <p data-testid="text">4</p>
+          <p data-testid="text">5</p>
+        </GenericSwiper>
+      </Wrapper>
+    );
+    const allText = getAllByTestId('text');
+    expect(allText).toHaveLength(5);
+  });
+});
