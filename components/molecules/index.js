0877eef2a87a3a4c30ba828bc4767dc331140663
BuildTools <williamcaetano14@gmail.com>
2021-12-03 21:44:00 -0300

    :sparkles: Created Section Tittle

--- components/molecules/index.js
+++ components/molecules/index.js
@@ -2,3 +2,4 @@ export { default as StreamerCard } from './StreamerCard';
 export { default as StreamersWrapper } from './StreamersWrapper';
 export { default as ColorModeSwitch } from './ColorModeSwitch';
 export { default as GenericSwiper } from './GenericSwiper';
+export { default as SectionTitle } from './SectionTitle';
